cafa544615c0bc8a3f70fafd5684f7c0766724c0
bowen9799 <bowen9799@gmail.com>
2018-06-05 22:44:30 +0800

    update 135.candy.java

---
+++ 135.candy.java
@@ -0,0 +1,68 @@
+/*
+ * [135] Candy
+ *
+ * https://leetcode.com/problems/candy/description/
+ *
+ * algorithms
+ * Hard (25.45%)
+ * Total Accepted:    79.4K
+ * Total Submissions: 312K
+ * Testcase Example:  '[1,0,2]'
+ *
+ * There are N children standing in a line. Each child is assigned a rating
+ * value.
+ * 
+ * You are giving candies to these children subjected to the following
+ * requirements:
+ * 
+ * 
+ * Each child must have at least one candy.
+ * Children with a higher rating get more candies than their neighbors.
+ * 
+ * 
+ * What is the minimum candies you must give?
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [1,0,2]
+ * Output: 5
+ * Explanation: You can allocate to the first, second and third child with 2,
+ * 1, 2 candies respectively.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [1,2,2]
+ * Output: 4
+ * Explanation: You can allocate to the first, second and third child with 1,
+ * 2, 1 candies respectively.
+ * ⁠            The third child gets 1 candy because it satisfies the above two
+ * conditions.
+ * 
+ * 
+ */
+public class Solution {
+    public int candy(int[] ratings) {
+        if (ratings == null || ratings.length == 0) return 0;
+        int total = 1, prev = 1, countDown = 0;
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i] >= ratings[i-1]) {
+                if (countDown > 0) {
+                    total += countDown*(countDown+1)/2; // arithmetic progression
+                    if (countDown >= prev) total += countDown - prev + 1;
+                    countDown = 0;
+                    prev = 1;
+                }
+                prev = ratings[i] == ratings[i-1] ? 1 : prev+1;
+                total += prev;
+            } else countDown++;
+        }
+        if (countDown > 0) { // if we were descending at the end
+            total += countDown*(countDown+1)/2;
+            if (countDown >= prev) total += countDown - prev + 1;
+        }
+        return total;
+    }
+}
